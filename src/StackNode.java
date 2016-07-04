7cd7607945714dd8895b186b670a6b7e6f3ad8b1
yulongtan <swifties@uw.edu>
2016-07-04 16:41:41 -0700

    added comments

--- src/StackNode.java
+++ src/StackNode.java
@@ -1,18 +1,25 @@
 /**
- * Created by Yulong on 3/31/2016.
+ *  Yulong Tan
+ *  3.31.16
+ *
+ *  Node class for LinkedStack
  */
-public class StackNode<E> {
-    public E data;
-    public StackNode next;
 
+public class StackNode<E> {
+    public E data;         // generic data for storage
+    public StackNode next; // reference to the next node
+
+    // Constructs a StackNode with null as its data
     public StackNode() {
         this(null);
     }
 
+    // Constructs a StackNode with the given data
     public StackNode(E e) {
         this(e, null);
     }
 
+    // Constructs a StackNode with the given data and its next node
     public StackNode(E e, StackNode next) {
         this.data = e;
         this.next = next;
